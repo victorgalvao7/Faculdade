e3c65271765a34f6d5f7453bf976a1c182c2768f
victor vieira galvao <victorgalvao582@gmail.com>
2026-04-27 19:54:00 -0300

    teste

--- pensamento computacional python/aula04-Repeticaoes/aula04a.py
+++ pensamento computacional python/aula04-Repeticaoes/aula04a.py
@@ -3,11 +3,15 @@ while cp <10:
     cp += 1
     if cp == 3:
         continue #ignora oque ta embaixo na estrutura de repeticao e continua
+    
+    if cp ==7:
+        break
 
     print(f"Produto {cp}")
+
 # While decrecente de 4 até 1
 
 i = 4
-while 1 > 0:
-    print("i")
+while i > 0:
+    print(i)
     i -= 1
