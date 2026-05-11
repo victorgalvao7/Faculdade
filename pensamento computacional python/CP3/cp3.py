936ae5eee422e630e4698c28e7567dfe3ac4bf03
victor vieira galvao <victorgalvao582@gmail.com>
2026-05-11 20:03:14 -0300

    Create cp3.py

---
+++ pensamento computacional python/CP3/cp3.py
@@ -0,0 +1,20 @@
+temperaturas = [[28, 31, 34, 33], [25, 27, 29, 28], [32, 35, 36, 34], [24, 26, 25, 27]]
+total = 0
+critico = 0
+media = 0
+maior = 0
+maior_media = 0
+for i in range(0, 4):
+    for j in range (0,4):
+        total += temperaturas[i][j]
+        if temperaturas[i][j] >= 33:
+            critico +=1
+    media = total/4
+    if media > maior_media:
+        maior = i
+    print(f"Sala {i+1}")
+    print(f"Média: {media}")
+    total = 0
+    print(f"Registros críticos: {critico}\n")
+    critico = 0
+print(f"Sala com maior risco: Sala {maior}")
