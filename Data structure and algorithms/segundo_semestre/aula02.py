e128ff44b889fbd03ac8175de7f3fea72561e4f1
victor vieira galvao <victorgalvao582@gmail.com>
2026-08-19 22:31:54 -0300

    oi

--- Data structure and algorithms/segundo_semestre/aula02.py
+++ Data structure and algorithms/segundo_semestre/aula02.py
@@ -9,3 +9,16 @@ def bubble_sort(lista):
     return lista
 
 print(bubble_sort(numeros))
+
+def selection_sort(lista):
+    n = len(lista)
+    for i in range(n):
+        menor = i
+        for j in range(i +1, n):
+            if lista[j] < lista[menor]:
+                menor = j
+        lista[i], lista[menor] = lista[menor], lista[i]
+        return lista
+
+print(bubble_sort(numeros))
+print(selection_sort(numeros))
